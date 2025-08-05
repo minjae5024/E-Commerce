cb4ab97ce6d32b28a29f1b7f3526facc72a8d537
minjae5024 <166333923+minjae5024@users.noreply.github.com>
2025-08-05 20:14:10 +0900

    Add files via upload

---
+++ src/main/java/minjae5024/contestProject/ContestProjectApplication.java
@@ -0,0 +1,13 @@
+package minjae5024.contestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestProjectApplication.class, args);
+	}
+
+}
