bad2e45b34e77d93a6102db6ecf0972d86518513
minjae5024 <minjae5024@naver.com>
2025-09-23 13:16:49 +0900

    0923

--- src/main/java/com/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -20,7 +20,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
